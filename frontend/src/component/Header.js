8246a53750dd6808cf98db681d3d71575f7f9c8c
Noahf99 <fourniernoah6@gmail.com>
2020-10-21 16:38:36 +0100

    Added frontend folder

---
+++ frontend/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                UCL Latex Generator
+            </h1>
+        </header>
+    )
+}
